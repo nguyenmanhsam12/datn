a2722d2cc2e3dbb1741c25d568a0ba4c30d7c989
nguyenmanhsam12 <nguyenmanhsam04@gmail.com>
2024-12-10 00:37:30 +0700

    fix conflix

--- resources/views/client/pages/home.blade.php
+++ resources/views/client/pages/home.blade.php
@@ -189,26 +189,6 @@
                                         </a>
                                         <a href="{{ route('getDetailProduct', ['slug' => $pr->slug]) }}"><img
                                                 src="{{ $pr->image }}" alt="" /></a>
-                                        <div class="product-action clearfix">
-                                            <a>
-                                                <form action="{{ route('wishlist.store') }}" method="POST"
-                                                    class="wishlist-form">
-                                                    @csrf
-                                                    <input type="hidden" name="product_id" value="{{ $pr->id }}">
-                                                    <button type="submit" class="btn-link wishlist-btn"
-                                                        data-bs-toggle="tooltip" data-bs-placement="top" title="Wishlist">
-                                                        <i class="zmdi zmdi-favorite-outline"></i>
-                                                    </button>
-                                                </form>
-                                            </a>
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#productModal{{ $pr->id }}"
-                                        {{-- <div class="product-action clearfix">
-                                            
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#productModal"
-                                                title="Quick View"><i class="zmdi zmdi-zoom-in"></i></a>
-                                            <a href="#" data-bs-toggle="tooltip" data-bs-placement="top"
-                                                title="Compare"><i class="zmdi zmdi-refresh"></i></a>
-                                        </div> --}}
                                     </div>
                                     <div class="product-info clearfix">
                                         <div class="fix">
@@ -228,62 +208,11 @@
                                             </span>
                                         </div>
                                     </div>
-                                </a>
-                            </div>                           
+</a>
+                            </div>
+
+
                             {{-- modal --}}
-                            <div class="modal fade" id="productModal{{$pr->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <div class="modal-product">
-                                                <div class="product-images">
-                                                    <div class="main-image images">
-                                                        <img alt="#" src="img/product/quickview-photo.webp"/>
-                                                    </div>
-                                                </div><!-- .product-images -->
-                       
-                                                <div class="product-info">
-                                                    <h1>Aenean eu tristique</h1>
-                                                    <div class="price-box-3">
-                                                        <hr />
-                                                        <div class="s-price-box">
-                                                            <span class="new-price">$160.00</span>
-                                                            <span class="old-price">$190.00</span>
-                                                        </div>
-                                                        <hr />
-                                                    </div>
-                                                    <a href="shop.html" class="see-all">See all features</a>
-                                                    <div class="quick-add-to-cart">
-                                                        <form method="post" class="cart">
-                                                            <div class="numbers-row">
-                                                                <input type="number" min="1" id="french-hens" value="3">
-                                                            </div>
-                                                            <button class="single_add_to_cart_button" type="submit">Add to cart</button>
-                                                        </form>
-                                                    </div>
-                                                    <div class="quick-desc">
-                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam fringilla augue nec est tristique auctor. Donec non est at libero.
-                                                    </div>
-                                                    <div class="social-sharing">
-                                                        <div class="widget widget_socialsharing_widget">
-                                                            <h3 class="widget-title-modal">Share this product</h3>
-                                                            <ul class="social-icons">
-                                                                <li><a target="_blank" title="Google +" href="#" class="gplus social-icon"><i class="zmdi zmdi-google-plus"></i></a></li>
-                                                                <li><a target="_blank" title="Twitter" href="#" class="twitter social-icon"><i class="zmdi zmdi-twitter"></i></a></li>
-                                                                <li><a target="_blank" title="Facebook" href="#" class="facebook social-icon"><i class="zmdi zmdi-facebook"></i></a></li>
-                                                                <li><a target="_blank" title="LinkedIn" href="#" class="linkedin social-icon"><i class="zmdi zmdi-linkedin"></i></a></li>
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                </div><!-- .product-info -->
-                                            </div><!-- .modal-product -->
-                                        </div><!-- .modal-body -->
-                                    </div><!-- .modal-content -->
-                                </div><!-- .modal-dialog -->
-                           </div>
                         @endforeach
 
                         <!-- Single-product end -->
